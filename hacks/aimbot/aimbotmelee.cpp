a51d783f03d44b8dc5aa2f74475afd9e62f4b716
mlemlody <garciaisheree@gmail.com>
2025-09-25 18:19:59 +0200

    Update aimbotmelee.cpp
    Update aimbotmelee.cpp

--- hacks/aimbot/aimbotmelee.cpp
+++ hacks/aimbot/aimbotmelee.cpp
@@ -4,17 +4,8 @@
 #include "../../gui/config.hpp"
 
 static float get_melee_range(Weapon* weapon) {
-  // Defaults: most stock melees ~48u; swords have extended reach ~70-75u.
   switch (weapon->get_def_id()) {
-  // Sniper melees
-  case Sniper_t_Kukri:
-  case Sniper_t_KukriR:
-  case Sniper_t_TheTribalmansShiv:
-  case Sniper_t_TheBushwacka:
-  case Sniper_t_TheShahanshah:
-    return 48.0f;
 
-  // Demoman swords (extended reach)
   case Demoman_t_TheEyelander:
   case Demoman_t_TheClaidheamhMor:
   case Demoman_t_ThePersianPersuader:
@@ -22,16 +13,9 @@ static float get_melee_range(Weapon* weapon) {
   case Demoman_t_TheScotsmansSkullcutter:
   case Demoman_t_HorselessHeadlessHorsemannsHeadtaker:
   case Demoman_t_NessiesNineIron:
-    return 70.0f;
-
-  // Demoman other melees (stock/caber/train)
-  case Demoman_t_Bottle:
-  case Demoman_t_BottleR:
-  case Demoman_t_UllapoolCaber:
-  case Demoman_t_ThePainTrain:
-    return 48.0f;
+    return 120.0f;
   }
-  return 48.0f;
+  return 84.0f;
 }
 
 void aimbot_melee(user_cmd* user_cmd, Player* localplayer, Weapon* weapon, Vec3 original_view_angles) {
@@ -49,16 +33,9 @@ void aimbot_melee(user_cmd* user_cmd, Player* localplayer, Weapon* weapon, Vec3 
 
   float range = get_melee_range(weapon);
   float dist_to_target = distance_3d(eye, target_pos);
-  if (dist_to_target > range + 4.0f)
+  if (dist_to_target > range + 10.0f) // buffer accounts for anim desync/latency
     return;
 
-  float x = remainderf(view_angles.x - original_view_angles.x, 360.0f);
-  float y = remainderf(view_angles.y - original_view_angles.y, 360.0f);
-  float clamped_x = x > 89.0f ? 89.0f : x < -89.0f ? -89.0f : x;
-  float clamped_y = y > 180.0f ? 180.0f : y < -180.0f ? -180.0f : y;
-  float fov = hypotf(clamped_x, clamped_y);
-  if (fov > 60.0f)
-    return;
 
   if (!is_player_visible(localplayer, target_player, bone))
     return;
